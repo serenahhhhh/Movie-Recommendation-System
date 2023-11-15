f3af53abdca2c316d6e68886042b91aec9272d9a
Pranjal Asthana <92260070+PranjalAsthana@users.noreply.github.com>
2023-11-15 16:19:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.header("Movie Recommendation System")
 select_value=st.selectbox("Select movie from dropdown",movies_list)
 
 def fetch_poster(movies_id):
-    url="https://api.themoviedb.org/3/movie/157336?api_key=2a0eae0a93a6347472a910bacaf3cae3&append_to_response=videos".format(movies_id)
+    url="https://api.themoviedb.org/3/movie/{}?api_key=2a0eae0a93a6347472a910bacaf3cae3&append_to_response=videos".format(movies_id)
     data=requests.get(url)
     data=data.json()
     poster_path=data['poster_path']
@@ -34,17 +34,17 @@ if st.button("Show Recommend"):
     movie_name, movie_poster=give_sim(select_value)
     col1,col2,col3,col4,col5=st.columns(5)
     with col1:
-        st.text(movie_name[0])
-        st.image(movie_poster[0])
+        st.image(posters[0])
+        st.write(names[0])
     with col2:
-        st.text(movie_name[1])
-        st.image(movie_poster[1])
+        st.image(posters[1])
+        st.write(names[1])
     with col3:
-        st.text(movie_name[2])
-        st.image(movie_poster[2])
+        st.image(posters[2])
+        st.write(names[2])
     with col4:
-        st.text(movie_name[3])
-        st.image(movie_poster[3])
+        st.image(posters[3])
+        st.write(names[3])
     with col5:
-        st.text(movie_name[4])
-        st.image(movie_poster[4])
+        st.image(posters[4])
+        st.write(names[4])
